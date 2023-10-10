39cfc398ff9fc5ca9320dcb5dbb78b824c66923f
BugaPunks <143012473+BugaPunks@users.noreply.github.com>
2023-10-10 00:00:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
 	weatherKey: '19c0e35c7a0934bde92fcb94b02c4b7e', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
+	language: 'sp', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '-16.527',
